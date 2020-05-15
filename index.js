5686706edeb4c7b5c73b50575d33216a2a2f0f23
Jorge Ivan Morales Salazar <jorge.morales@CO-IT005960.local>
2020-05-14 19:37:20 -0500

    Add bodyParser and conditionals for methods

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const http = require('http')
+const { bodyParser } = require('./lib/bodyParser')
 
-const server = http.createServer((req, res) => {
-   res.writeHead(200, {'Content-Type':'application/json'})
-   res.write(JSON.stringify({ message: 'Response from server' }))
-   res.end()
+const server = http.createServer(async (req, res) => {
+   const { url, method } = req;
+   if (method=='GET') {
+      res.writeHead(200, {'Content-Type':'application/json'})
+      res.write(JSON.stringify({ message: 'Response from server for GET Method' }))
+      res.end()
+   } else if (method == 'POST')  {
+      await bodyParser(req)
+      res.writeHead(200, {'Content-Type':'application/json'})
+      res.write(JSON.stringify({ message: 'Response from server for POST Method', content_task: req.body.task }))
+      res.end()
+   }
  })
 
  server.listen(3000)
